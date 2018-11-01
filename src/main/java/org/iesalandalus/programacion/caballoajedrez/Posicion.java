ce1515a7c56ed14a372253fff2789ff9fc0c6544
Manuel <Manuel@casa>
2018-11-01 21:15:51 +0100

    Creo los setter y getter para los atributos de posicion, con validacion de que fila sean los numeros del 1 al 8 ( ambos inclusive) y las columnas de la "a" a la "h"

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -11,7 +11,38 @@ package org.iesalandalus.programacion.caballoajedrez;
  */
 public class Posicion 
 {
-    int fila;
-    char columna;
+    //declaracion de variables
+    private int fila;
+    private char columna;
     
+    //creo los metodos get y set para los atributos
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if (fila>=1 && fila<=8)
+        {
+            this.fila = fila;
+        }else  
+          
+        {
+            throw new IllegalArgumentException("El valor fila esta fuera de su rango válido (1-8)");
+        }
+    }
+    
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna) {
+        
+        if (columna==a || columna==b || columna==c || columna==d || columna==e || columna==f || columna==g || columna==h)
+        {
+        this.columna = columna;
+    } else
+            throw new IllegalArgumentException("El valor columna esta fuera de su rango válido (a-h)");
+        }
 }
