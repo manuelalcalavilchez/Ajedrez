01f80242916ecf78f9d32fd75fdac7019dca5237
Manuel <Manuel@casa>
2018-11-01 21:46:51 +0100

    creo el constructor con parametros, y asigno los parametros a traves del los metodos set que nos validan los parametros y dan error de excepcion en caso de que no se cumplan las condiciones

--- src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/caballoajedrez/Posicion.java
@@ -45,4 +45,24 @@ public class Posicion
     } else
             throw new IllegalArgumentException("El valor columna esta fuera de su rango válido (a-h)");
         }
+    
+    //contructor que acepte como parámetros la fila y la columna y los asigne a los atributos si son correctos, 
+    //y si no, que lance una excepción indicando el problema.
+    /**
+     * Constructor que inicializa todos los atributos a través de parámetros
+     * @param fila 
+     * @param columna 
+     */
+    public Posicion (int fila,char columna)
+    {
+        
+        /*Llamamos al método que permite validar los parametos de fila
+         y columna   */
+        setFila(fila);
+        setColumna(columna);
+    }
+
+
+
+
 }
